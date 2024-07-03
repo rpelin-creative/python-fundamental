204af6a12e0cbd44ce8c26fccc2e67fb5c3ebde6
zeads <ruri.pelinandang@gmail.com>
2024-07-03 08:50:09 +0700

    Penggunaan list for in dan index

---
+++ dasar_sintak_tipe_data_list.py
@@ -0,0 +1,11 @@
+daftar_buku = ['Seven habits','How to Influence People','First Things First']
+print('Tampilkan variable daftar_buku')
+print(daftar_buku)
+
+print('\nAmbil data menggunakan for in')
+for buku in daftar_buku:
+    print(buku)
+
+print('\nTampilkan isi daftar_buku berdasarkan index tertentu')
+print(daftar_buku[0])
+print(daftar_buku[2])
