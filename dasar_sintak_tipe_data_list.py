2f4f706a62b562eba1c1ea8f23698a85c63157ad
zeads <ruri.pelinandang@gmail.com>
2024-07-03 09:14:48 +0700

    Penggunaan list for in dan index penambahan data menggunakan append ke list

--- dasar_sintak_tipe_data_list.py
+++ dasar_sintak_tipe_data_list.py
@@ -1,4 +1,4 @@
-daftar_buku = ['Seven habits','How to Influence People','First Things First']
+daftar_buku = ['Seven habits','How to Influence People','First Things First','4DX']
 print('Tampilkan variable daftar_buku')
 print(daftar_buku)
 
@@ -9,3 +9,19 @@ for buku in daftar_buku:
 print('\nTampilkan isi daftar_buku berdasarkan index tertentu')
 print(daftar_buku[0])
 print(daftar_buku[2])
+
+print('\nTampilkan dengan for in range')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+daftar_buku = [1, 'Kenji volume 2', -345, 3.14]
+print('\nTampilkan dengan for in range dengan isi type data berbeda beda ')
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nKembalikan nilai awal daftar buku')
+daftar_buku = ['Aku','Bandung','Cacing','Dodol']
+print('\ndaftar_buku setelah di append')
+daftar_buku.append('Endog')
+for i in range(0,len(daftar_buku)):
+    print(daftar_buku[i])
